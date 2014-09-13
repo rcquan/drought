44de7bf342e8e9a1d961efda6797495b0f15448a
rcquan <ryan.quan08@gmail.com>
2014-09-13 03:47:28 -0400

    download and decompress gzip into data folder

--- noaa_drought_munge.R
+++ noaa_drought_munge.R
@@ -1,11 +1,68 @@
-setwd("/Users/Quan/GitHub/drought/")
+#############################
+# Ryan Quan
+# NOAA Drought Data
+# 
+# 2014-09-15
+#
+# The following code gets
+# data from NOAA
+#############################
 library(dplyr)
+library(rJava)
+library(stringr)
+library(GEOquery)
 
+
+setwd("/Users/Quan/GitHub/drought/")
+
+dir.create("data")
+baseURL <- "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/2014/"
+
+#############################
+# DOWNLOADING THE DATA
+#############################
+if(!file.exists("ish_history.csv")){
+    download.file("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/ish-history.csv",
+                  "ish_history.csv")
+}
 df <- read.csv("ish-history.csv", stringsAsFactors = FALSE)
+
+# change column names to lowercase
 names(df) <- tolower(names(df))
 
+# constructing the url path
 df_ca <- df %>%
+    # get only stations in US and CA
     filter(ctry == "US" & state == "CA") %>%
-    mutate(filename = sprintf("%s-%s-2014.gz", usaf, wban)) %>%
-    group_by(state) %>%
-    summarise(count = n())
+    filter(str_detect(end, "2014[0-9]*")) %>%
+    mutate(file = sprintf("%s-%s-2014.gz", usaf, wban),
+           url = sprintf("%s%s-%s-2014.gz", baseURL, usaf, wban)) %>%
+    select(file, url)
+
+file <- df_ca$file
+url <- df_ca$url
+
+# get data from NOAA website
+setwd("data")
+dwnld_errors <- vector()
+for(i in 1:length(file)){
+    if(!file.exists(file)){
+        # ERROR HANDLING
+        tryCatch({
+            download.file(url[i], file[i])
+            # line unable to read in data to dwnld_errors
+        }, error = function(e){dwnld_errors <- append(dwnld_errors, file[i])})
+    } 
+}
+
+#############################
+# DATA PROCESSING
+#############################
+
+# get list of files in directory
+zip_file <- list.files(pattern = ".gz")
+# unzip the .gz file
+sapply(zip_file, gunzip)
+
+
+#-------JAVA CODE HERE ---------#
